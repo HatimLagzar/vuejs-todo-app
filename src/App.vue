ac29d22f208ddab1f2ab39be7037af44014cc7f2
Hatim Lagzar <LHatimDev@gmail.com>
2022-04-13 22:39:29 +0000

    Load todo list from a third party api

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,46 @@
 <template>
+  <div class="container">
+    <h1>Todo App</h1>
+    <TodoItem v-for="todo in todoList" :todo="todo" :key="todo.id" />
+  </div>
 </template>
 
 <script>
+import TodoItem from './components/TodoItem.vue'
+
 export default {
   name: 'App',
+  components: {
+    TodoItem
+  },
+  data() {
+    return {
+      todoList: []
+    }
+  },
+  created() {
+    this.loadTodoList()
+      .then(response => {
+        this.todoList = response
+      })
+  },
+  methods: {
+    loadTodoList() {
+      return fetch('https://jsonplaceholder.typicode.com/todos')
+        .then(response => response.json())
+        .catch(error => console.log(error))
+    }
+  }
 }
 </script>
+<style lang="css">
+  body {
+    font-size: 16px;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  .container {
+    max-width: 700px;
+    margin: 0 auto;
+  }
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="todo-item">
+        <p>{{todo.title}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TodoItem',
+    props: {
+        todo: Object
+    }
+}
+</script>
+
+<style lang="css" scoped>
+    .todo-item {
+        padding: 5px 20px;
+        border-radius: 10px;
+        border: 1px solid #DDD;
+        margin-bottom: 10px;
+    }
+</style>
